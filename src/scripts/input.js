1865e117d07b002b279bc6dd0ec5ba93edfe543e
Adria Blancafort <adriablancafort@gmail.com>
2024-03-07 22:01:31 +0100

    instant image loading

--- src/scripts/input.js
+++ src/scripts/input.js
@@ -208,7 +208,7 @@ const topPluginsTitle = `
 
 const detectWpSuccess = (websiteName) => `
 <div class="card card__wp border">
-  <img src="/wordpress-detector-success.svg" alt="Success Logo" class="card--icon" width="75" height="60" />
+  <img src="/wordpress-detector-success.svg" alt="Success Logo" class="card--icon" width="75" height="60" loading="eager" />
   <div>
     <h4 class="card--title cart--title__success">Hurray!</h4>
     <p><strong>${websiteName}</strong> is using WordPress!</p>
@@ -217,7 +217,7 @@ const detectWpSuccess = (websiteName) => `
 
 const detectWpFail = (websiteName) => `
 <div class="card card__wp border">
-  <img src="/wordpress-detector-fail.svg" alt="Success Logo" class="card--icon" width="75" height="60" />
+  <img src="/wordpress-detector-fail.svg" alt="Success Logo" class="card--icon" width="75" height="60" loading="eager" />
   <div>
     <h4 class="card--title cart--title__fail">Bad news...</h4>
     <p><strong>${websiteName}</strong> is not using WordPress.</p>
@@ -226,7 +226,7 @@ const detectWpFail = (websiteName) => `
 
 const htmlRetrieveError = (websiteName) => `
 <div class="card card__wp border">
-  <img src="/html-retrieve-error.svg" alt="Success Logo" class="card--icon" width="75" height="60" />
+  <img src="/html-retrieve-error.svg" alt="Success Logo" class="card--icon" width="75" height="60" loading="eager" />
   <div>
     <h4 class="card--title cart--title__fail">Bad news...</h4>
     <p> We could not retrieve the HTML from: <strong>${websiteName}</strong>.</p>
