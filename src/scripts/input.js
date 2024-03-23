4adb74c197da504e514617c8c50ee0c995ca2062
Adria Blancafort <adriablancafort@gmail.com>
2024-03-23 23:34:39 +0100

    embeded svg

--- src/scripts/input.js
+++ src/scripts/input.js
@@ -1,33 +1,33 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  const inputForm = document.getElementById('inputForm');
-  const outputContainer = document.getElementById('outputContainer');
+  const inputForm = document.getElementById("inputForm");
+  const outputContainer = document.getElementById("outputContainer");
 
-  let oldUrl = '';
+  let oldUrl = "";
 
-  inputForm.addEventListener('submit', (event) => {
+  inputForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
     // Get the inputUrl url
-    let inputUrl = document.getElementById('inputUrl').value;
+    let inputUrl = document.getElementById("inputUrl").value;
 
     if (oldUrl !== inputUrl) {
       const websiteName = formatWebsiteName(inputUrl);
 
       // Wordpress Detected container
-      outputContainer.innerHTML = '';
-      const wpContainer = document.createElement('div');
+      outputContainer.innerHTML = "";
+      const wpContainer = document.createElement("div");
       outputContainer.appendChild(wpContainer);
       wpContainer.innerHTML = analyzingResultsTitle(websiteName);
-      wpContainer.innerHTML += detectWpSkeleton; 
+      wpContainer.innerHTML += detectWpSkeleton;
 
       // Themes Detected container
-      const themesContainer = document.createElement('div');
+      const themesContainer = document.createElement("div");
       outputContainer.appendChild(themesContainer);
       themesContainer.innerHTML = analyzingThemesTitle(websiteName);
       themesContainer.innerHTML += detectThemesSkeleton;
 
       // Plugins Detected container
-      const pluginsContainer = document.createElement('div');
+      const pluginsContainer = document.createElement("div");
       outputContainer.appendChild(pluginsContainer);
       pluginsContainer.innerHTML = analyzingPluginsTitle(websiteName);
       pluginsContainer.innerHTML += detectPluginsSkeleton;
@@ -36,91 +36,90 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
       if (validateUrl(inputUrl)) {
         oldUrl = inputUrl;
-        inputUrl = sanatizeUrl(inputUrl); 
+        inputUrl = sanatizeUrl(inputUrl);
 
-        apiRequest(inputUrl, "wp").then(data => {
-          wpContainer.innerHTML = showingResultsTitle(websiteName);      
+        apiRequest(inputUrl, "wp")
+          .then((data) => {
+            wpContainer.innerHTML = showingResultsTitle(websiteName);
 
-          if (data.wp === true) {
-            wpContainer.innerHTML += detectWpSuccess(websiteName);
+            if (data.wp === true) {
+              wpContainer.innerHTML += detectWpSuccess(websiteName);
 
-            apiRequest(inputUrl, "themes").then(data => {
-              themesContainer.innerHTML = detectThemesTitle(websiteName);
+              apiRequest(inputUrl, "themes").then((data) => {
+                themesContainer.innerHTML = detectThemesTitle(websiteName);
 
-              if (data.themes) {
-                data.themes.forEach(theme => {
-                  const themeCard = document.createElement('div');
+                if (data.themes) {
+                  data.themes.forEach((theme) => {
+                    const themeCard = document.createElement("div");
+                    themeCard.innerHTML = detectThemesCard(theme);
+                    if (theme.link) {
+                      themeCard.addEventListener("click", () => {
+                        window.open(theme.link, "_blank");
+                      });
+                    }
+                    themesContainer.appendChild(themeCard);
+                  });
+                } else {
+                  themesContainer.innerHTML += noThemesDetected(websiteName);
+                }
+              });
+
+              apiRequest(inputUrl, "plugins").then((data) => {
+                pluginsContainer.innerHTML = detectPluginsTitle(websiteName);
+
+                if (data.plugins) {
+                  data.plugins.forEach((plugin) => {
+                    const pluginCard = document.createElement("div");
+                    pluginCard.innerHTML = detectPluginsCard(plugin);
+                    if (plugin.link) {
+                      pluginCard.addEventListener("click", () => {
+                        window.open(plugin.link, "_blank");
+                      });
+                    }
+                    pluginsContainer.appendChild(pluginCard);
+                  });
+                } else {
+                  pluginsContainer.innerHTML += noPluginsDetected(websiteName);
+                }
+              });
+            } else if (data.wp === false) {
+              wpContainer.innerHTML += detectWpFail(websiteName);
+
+              apiRequest(inputUrl, "top-themes").then((data) => {
+                themesContainer.innerHTML = topThemesTitle;
+                data.themes.forEach((theme) => {
+                  const themeCard = document.createElement("div");
                   themeCard.innerHTML = detectThemesCard(theme);
                   if (theme.link) {
-                    themeCard.addEventListener('click', () => {
-                      window.open(theme.link, '_blank');
+                    themeCard.addEventListener("click", () => {
+                      window.open(theme.link, "_blank");
                     });
                   }
                   themesContainer.appendChild(themeCard);
                 });
-              } else {
-                themesContainer.innerHTML += noThemesDetected(websiteName);
-              }
-            });
+              });
 
-            apiRequest(inputUrl, "plugins").then(data => {
-              pluginsContainer.innerHTML = detectPluginsTitle(websiteName);
-
-              if (data.plugins) {
-                data.plugins.forEach(plugin => {
-                  const pluginCard = document.createElement('div');
+              apiRequest(inputUrl, "top-plugins").then((data) => {
+                pluginsContainer.innerHTML = topPluginsTitle;
+                data.plugins.forEach((plugin) => {
+                  const pluginCard = document.createElement("div");
                   pluginCard.innerHTML = detectPluginsCard(plugin);
                   if (plugin.link) {
-                    pluginCard.addEventListener('click', () => {
-                      window.open(plugin.link, '_blank');
+                    pluginCard.addEventListener("click", () => {
+                      window.open(plugin.link, "_blank");
                     });
                   }
                   pluginsContainer.appendChild(pluginCard);
                 });
-              } else {
-                pluginsContainer.innerHTML += noPluginsDetected(websiteName);
-              }
-            });
-
-          } else if (data.wp === false) {
-            wpContainer.innerHTML += detectWpFail(websiteName);
-            
-            apiRequest(inputUrl, "top-themes").then(data => {
-              themesContainer.innerHTML = topThemesTitle;         
-              data.themes.forEach(theme => {
-                const themeCard = document.createElement('div');
-                themeCard.innerHTML = detectThemesCard(theme);
-                if (theme.link) {
-                  themeCard.addEventListener('click', () => {
-                    window.open(theme.link, '_blank');
-                  });
-                }
-                themesContainer.appendChild(themeCard);
               });
-            });
-
-            apiRequest(inputUrl, "top-plugins").then(data => {
-              pluginsContainer.innerHTML = topPluginsTitle;
-              data.plugins.forEach(plugin => {
-                const pluginCard = document.createElement('div');
-                pluginCard.innerHTML = detectPluginsCard(plugin);
-                if (plugin.link) {
-                  pluginCard.addEventListener('click', () => {
-                    window.open(plugin.link, '_blank');
-                  });
-                }
-                pluginsContainer.appendChild(pluginCard);
-              });
-            });
-          }
-
-        }).catch(() => {
-          outputContainer.innerHTML = showingResultsTitle(websiteName);  
-          outputContainer.innerHTML += htmlRetrieveError(websiteName);
-        });
-
+            }
+          })
+          .catch(() => {
+            outputContainer.innerHTML = showingResultsTitle(websiteName);
+            outputContainer.innerHTML += htmlRetrieveError(websiteName);
+          });
       } else {
-        oldUrl = '';
+        oldUrl = "";
         outputContainer.innerHTML = invalidUrl;
       }
     }
@@ -130,33 +129,38 @@ document.addEventListener("DOMContentLoaded", (event) => {
 // Functions
 
 const formatWebsiteName = (url) => {
-  url = url.replace(/^(https?:\/\/)?/, '');
-  url = url.replace(/\/+$/, '');
+  url = url.replace(/^(https?:\/\/)?/, "");
+  url = url.replace(/\/+$/, "");
   url = url.slice(0).toLowerCase();
   return url;
-}
+};
 
 const validateUrl = (string) => {
-  let pattern = new RegExp('^(https?:\\/\\/)?' + // validate protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
-    '(\\#[-a-z\\d_]*)?$', 'i'); // validate fragment locator
+  let pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // validate protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // validate fragment locator
   return !!pattern.test(string);
-}
+};
 
 const sanatizeUrl = (url) => {
-  url = url.replace(/\/+$/, ''); // Remove / at the end of the URL if needed
-  if (!/^https?:\/\//i.test(url)) { // Add https:// if needed
-    url = 'https://' + url;          
+  url = url.replace(/\/+$/, ""); // Remove / at the end of the URL if needed
+  if (!/^https?:\/\//i.test(url)) {
+    // Add https:// if needed
+    url = "https://" + url;
   }
   return url;
 };
 
 const apiRequest = (inputUrl, type) => {
-  return fetch(`https://wp-detector.000webhostapp.com/?url=${inputUrl}&type=${type}`)
-    .then(response => response.json());
+  return fetch(
+    `https://wp-detector.000webhostapp.com/?url=${inputUrl}&type=${type}`
+  ).then((response) => response.json());
 };
 
 // Elements
@@ -208,7 +212,11 @@ const topPluginsTitle = `
 
 const detectWpSuccess = (websiteName) => `
 <div class="card card__wp border">
-  <img src="/wordpress-detector-success.svg" alt="Success Logo" class="card--icon" width="75" height="60" loading="eager" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38.785 32.418" class="card--icon" width="75" height="60">
+      <path fill="#464342" d="M2.304 16.208c0 5.504 3.199 10.26 7.837 12.515L3.508 10.549a13.852 13.852 0 0 0-1.204 5.66zm23.292-.701c0-1.719-.617-2.909-1.147-3.835-.705-1.146-1.365-2.116-1.365-3.26 0-1.28.969-2.469 2.335-2.469.061 0 .12.008.18.011a13.854 13.854 0 0 0-9.39-3.65c-4.858 0-9.132 2.492-11.618 6.267.326.01.634.017.895.017 1.454 0 3.706-.176 3.706-.176.75-.045.838 1.056.089 1.145 0 0-.753.089-1.592.133l5.064 15.061 3.043-9.126-2.167-5.936a25.336 25.336 0 0 1-1.458-.133c-.749-.043-.661-1.19.088-1.145 0 0 2.297.177 3.663.177 1.454 0 3.706-.177 3.706-.177.75-.044.838 1.057.089 1.145 0 0-.755.09-1.592.133l5.025 14.947 1.387-4.634c.601-1.924 1.059-3.305 1.059-4.495z M16.453 17.425 12.28 29.548a13.904 13.904 0 0 0 8.545-.222 1.221 1.221 0 0 1-.099-.192zM28.41 9.537c.06.443.094.919.094 1.43 0 1.411-.264 2.997-1.058 4.98L23.2 28.227c4.134-2.41 6.914-6.889 6.914-12.018 0-2.418-.617-4.691-1.703-6.672z M16.209 0C7.272 0 0 7.271 0 16.208c0 8.939 7.272 16.21 16.209 16.21s16.21-7.271 16.21-16.21C32.419 7.271 25.146 0 16.209 0Zm0 31.675C7.68 31.675.743 24.736.743 16.208.743 7.681 7.681.743 16.21.743c8.527 0 15.465 6.938 15.465 15.465 0 8.528-6.938 15.467-15.465 15.467z"/>
+    <circle cx="32.836" cy="23.094" r="5.117" fill="#fff" paint-order="markers stroke fill"/>
+    <path fill="#02d957" d="M38.785 23.42a6.068 6.068 0 1 1-6.068-6.067 6.067 6.067 0 0 1 6.068 6.067zm-3.146-3.447a.606.606 0 0 0-.83.222l-2.933 5.082-1.328-1.594a.607.607 0 1 0-.932.777l1.884 2.262a.607.607 0 0 0 .466.219.636.636 0 0 0 .052-.002.606.606 0 0 0 .474-.301l3.368-5.836a.606.606 0 0 0-.222-.829z"/>
+  </svg>
   <div>
     <h4 class="card--title cart--title__success">Hurray!</h4>
     <p><strong>${websiteName}</strong> is using WordPress!</p>
@@ -217,7 +225,11 @@ const detectWpSuccess = (websiteName) => `
 
 const detectWpFail = (websiteName) => `
 <div class="card card__wp border">
-  <img src="/wordpress-detector-fail.svg" alt="Success Logo" class="card--icon" width="75" height="60" loading="eager" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38.734 32.418" class="card--icon" width="75" height="60">
+      <path fill="#464342" d="M2.304 16.208c0 5.504 3.199 10.26 7.837 12.515L3.508 10.549a13.852 13.852 0 0 0-1.204 5.66zm23.292-.701c0-1.719-.617-2.909-1.147-3.835-.705-1.146-1.365-2.116-1.365-3.26 0-1.28.969-2.469 2.335-2.469.061 0 .12.008.18.011a13.854 13.854 0 0 0-9.39-3.65c-4.858 0-9.132 2.492-11.618 6.267.326.01.634.017.895.017 1.454 0 3.706-.176 3.706-.176.75-.045.838 1.056.089 1.145 0 0-.753.089-1.592.133l5.064 15.061 3.043-9.126-2.167-5.936a25.336 25.336 0 0 1-1.458-.133c-.749-.043-.661-1.19.088-1.145 0 0 2.297.177 3.663.177 1.454 0 3.706-.177 3.706-.177.75-.044.838 1.057.089 1.145 0 0-.755.09-1.592.133l5.025 14.947 1.387-4.634c.601-1.924 1.059-3.305 1.059-4.495z M16.453 17.425 12.28 29.548a13.904 13.904 0 0 0 8.545-.222 1.221 1.221 0 0 1-.099-.192zM28.41 9.537c.06.443.094.919.094 1.43 0 1.411-.264 2.997-1.058 4.98L23.2 28.227c4.134-2.41 6.914-6.889 6.914-12.018 0-2.418-.617-4.691-1.703-6.672z M16.209 0C7.272 0 0 7.271 0 16.208c0 8.939 7.272 16.21 16.209 16.21s16.21-7.271 16.21-16.21C32.419 7.271 25.146 0 16.209 0Zm0 31.675C7.68 31.675.743 24.736.743 16.208.743 7.681 7.681.743 16.21.743c8.527 0 15.465 6.938 15.465 15.465 0 8.528-6.938 15.467-15.465 15.467z"/>
+      <circle cx="32.836" cy="23.094" r="5.117" fill="#fff" paint-order="markers stroke fill"/>
+      <path fill="#ff2854" d="M38.734 23.348a6.008 6.008 0 1 1-6.008-6.008 6.007 6.007 0 0 1 6.008 6.008zm-5.163.008 2.3-2.302a.601.601 0 0 0-.85-.85l-2.3 2.302-2.302-2.301a.601.601 0 0 0-.85.85l2.303 2.301-2.302 2.301a.601.601 0 0 0 .85.85l2.3-2.302 2.302 2.302a.601.601 0 0 0 .85-.85z"/>
+  </svg>
   <div>
     <h4 class="card--title cart--title__fail">Bad news...</h4>
     <p><strong>${websiteName}</strong> is not using WordPress.</p>
@@ -226,7 +238,11 @@ const detectWpFail = (websiteName) => `
 
 const htmlRetrieveError = (websiteName) => `
 <div class="card card__wp border">
-  <img src="/html-retrieve-error.svg" alt="Success Logo" class="card--icon" width="75" height="60" loading="eager" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36.277 32.736" class="card--icon" width="75" height="60">
+    <path fill="#464342" fill-rule="evenodd" d="M23.627 9.82H9.513l.337 3.274h13.442c-.197 2.868-.793 8.944-1.012 11.708l-7.55 2.125-.016.016-7.555-2.658-.518-6.28h3.704l.262 3.377 4.113 1.533h.01l4.107-1.473.423-5.074H6.476c-.062-.656-.856-9.18-.995-9.82h18.477c-.104 1.08-.209 2.19-.33 3.273ZM0 0l2.681 29.705 12.033 3.031 12.064-3.092L29.462 0Z"/>
+    <circle cx="30.379" cy="20.78" r="5.117" fill="#fff" paint-order="markers stroke fill"/>
+    <path fill="#ff2854" d="M36.277 21.034a6.008 6.008 0 1 1-6.008-6.008 6.007 6.007 0 0 1 6.008 6.008zm-5.163.008 2.3-2.302a.601.601 0 0 0-.85-.85l-2.3 2.302-2.302-2.301a.601.601 0 0 0-.85.85l2.303 2.301-2.302 2.301a.601.601 0 0 0 .85.85l2.3-2.302 2.302 2.302a.601.601 0 0 0 .85-.85z"/>
+  </svg>
   <div>
     <h4 class="card--title cart--title__fail">Bad news...</h4>
     <p> We could not retrieve the HTML from: <strong>${websiteName}</strong>.</p>
@@ -249,7 +265,9 @@ const detectThemesCard = (theme) => `
     <h4 class="card--title">${theme.title}</h4>
     <p>Author: <strong>${theme.author}</strong></p>
     <p>Version: <span class="badge">${theme.version}</span></p>
-    <p>Website: <a href="${theme.website}" target="_blank">${theme.sanatizedWebsite}</a></p>
+    <p>Website: <a href="${theme.website}" target="_blank">${
+      theme.sanatizedWebsite
+    }</a></p>
     <p>WordPress Version: <strong>${theme.reqWpVersion} or higher</strong></p>
     <p>Tested up To: <strong>${theme.testedWpVersion}</strong></p>
     <p>PHP Version: <strong>${theme.reqPhpVersion} or higher</strong></p>
@@ -257,7 +275,9 @@ const detectThemesCard = (theme) => `
       <strong>Description:</strong> ${theme.description}
     </p>
   </div>
-  ${theme.link ? `
+  ${
+    theme.link
+      ? `
   <div class="cart--read-more--container">
     <span class="cart--read-more">
       <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" height="25px" width="25px" >
@@ -265,7 +285,9 @@ const detectThemesCard = (theme) => `
       </svg>
       Read more
     </span>
-  </div>` : ''}
+  </div>`
+      : ""
+  }
 </div>`;
 
 const detectThemesSkeleton = `
@@ -306,12 +328,16 @@ const detectPluginsCard = (plugin) => `
   <img src="${plugin.banner}" alt="Plugin Banner" class="card--banner" />
   <div class="card--info-container">
     <div class="card--title-container__plugin">
-      <img src="${plugin.icon}" alt="Plugin Icon" class="card--icon" width="60px" height="60px" />
+      <img src="${
+        plugin.icon
+      }" alt="Plugin Icon" class="card--icon" width="60px" height="60px" />
       <h4 class="card--title">${plugin.title}</h4>
     </div>
     <p>Author: <strong>${plugin.author}</strong></p>
     <p>Version: <span class="badge">${plugin.version}</span></p>
-    <p>Website: <a href="${plugin.website}" target="_blank">${plugin.sanatizedWebsite}</a></p>
+    <p>Website: <a href="${plugin.website}" target="_blank">${
+      plugin.sanatizedWebsite
+    }</a></p>
     <p>WordPress Version: <strong>${plugin.reqWpVersion} or higher</strong></p>
     <p>Tested up To: <strong>${plugin.testedWpVersion}</strong></p>
     <p>PHP Version: <strong>${plugin.reqPhpVersion} or higher</strong></p>
@@ -319,7 +345,9 @@ const detectPluginsCard = (plugin) => `
       <strong>Description:</strong> ${plugin.description}
     </p>
   </div>
-  ${plugin.link ? `
+  ${
+    plugin.link
+      ? `
   <div class="cart--read-more--container">
     <span class="cart--read-more">
       <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" height="25px" width="25px" >
@@ -327,7 +355,9 @@ const detectPluginsCard = (plugin) => `
       </svg>
       Read more
     </span>
-  </div>` : ''}
+  </div>`
+      : ""
+  }
 </div>`;
 
 const detectPluginsSkeleton = `
