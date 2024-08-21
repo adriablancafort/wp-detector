a46bf89e521bf574177208f33fbdbba5c92de150
Adria Blancafort <adriablancafort@gmail.com>
2024-08-21 23:15:57 +0200

    added back trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://wp-detector.com",
-  trailingSlash: "never",
   integrations: [sitemap()],
 });
